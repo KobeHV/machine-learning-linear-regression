e17b8506cf1a42960063328e5924ee38587d3edb
Kobe <1329777148@qq.com>
2018-09-29 19:12:12 +0800

    divide data

--- polynomial fitting.py
+++ polynomial fitting.py
@@ -19,12 +19,13 @@ def func(x):
 for i in range(0,xnum):
     x=x+1.0/xnum
     a.append(x)
-    b.append(func(x))
-
-
+random.shuffle(a)#打乱数组a的顺序  训练集：开发集：测试集=8：1：1
+for i in range(0,xnum):
+    b.append(func(a[i]))
 #创建X,T,W矩阵
-m = 10#M阶数，可以更改
+m = 7#M阶数，可以更改
 n = len(a)
+divide = int(n/10*8)#训练集：开发集：测试集=8：1：1
 Xa = np.zeros(shape=(n,m+1))
 for i in range(0,n):
     for j in range(0,m+1):
@@ -35,46 +36,69 @@ for i in range(0,n):
     Ta[i] = b[i]
 T = np.mat(Ta)
 W = W = np.mat(np.ones(shape=(m + 1, 1)))
-
+X1 = X[0:divide,0:]#训练集
+X2 = X[divide:,0:]#开发集
+T1 = T[0:divide,0:]#训练集
+T2 = T[divide:,0:]#开发集
 
 #第二部分
 
+
+minlamda = math.exp(-15)
 #解析解
 def Mode1_1():
-    W = (X.T * X).I * X.T * T  # 不加惩罚项
+    W = (X1.T * X1).I * X1.T * T1  # 不加惩罚项
     return W
 def Mode1_2():
-    lamda = math.exp(-10)
-    I = np.eye(m + 1)  # 加惩罚项
-    Ie = lamda*I
-    W = (X.T * X + Ie).I * X.T * T
+    minloss = 10000
+    mindex = 0
+    Temp = np.mat(np.ones(shape=(m + 1, 1)))
+    for i in range(-100,100):
+        lamda = math.exp(i)
+        I = lamda*np.eye(m + 1)  # 加惩罚项
+        W = (X1.T * X1 + I).I * X1.T * T1
+        loss = 1/(2*(n-divide)) * ( (X2*W-T2).T*(X2*W-T2)+lamda*W.T*W )
+        if(loss<minloss):
+            minloss = loss
+            mindex = i
+            Temp = W
+    print("MinLoss=",minloss," Lamda=exp(",mindex,")")
+    W = Temp
     return W
 
 #梯度下降
 def Mode2_1():
-    alpha = 0.05  # 学习率
-    iterNum = 100000  # 迭代次数
+    alpha = 0.08  # 学习率
+    iterNum = 1000000  # 迭代次数
     W = np.mat(np.ones(shape=(m + 1, 1)))
     for i in range(0, iterNum):
         # E(W)对矩阵W的一阶导数,结果是一个矩阵
-        Differential = 1.0 / xnum * (X.T * X * W - X.T * T)# 不加惩罚项
+        Differential = 1.0 / divide * (X1.T * X1 * W - X1.T * T1)# 不加惩罚项
+        loss = 1 / (2 * divide) * ((X1 * W - T1).T * (X1 * W - T1) + minlamda * W.T * W)
+        if(loss<0.015):
+            print("迭代次数:", i)
+            break
         W = W - alpha * Differential
     return W
 def Mode2_2():
-    alpha = 0.05  # 学习率
-    iterNum = 100000  # 迭代次数
-    lamda = math.exp(-8)
+    alpha = 0.08  # 学习率
+    iterNum = 1000000  # 迭代次数
     W = np.mat(np.ones(shape=(m + 1, 1)))
     for i in range(0, iterNum):
         # E(W)对矩阵W的一阶导数,结果是一个矩阵
-        Differential = 1.0 / xnum * (X.T * X * W - X.T * T + lamda * W)# 加惩罚项
+        Differential = 1.0 / divide * (X1.T * X1 * W - X1.T * T1 + minlamda * W)# 加惩罚项
+        loss = 1 / (2 * divide) * ((X1 * W - T1).T * (X1 * W - T1) + minlamda * W.T * W)
+        if (loss < 0.015):
+            print("迭代次数:",i)
+            break
         W = W - alpha * Differential
+    print(" Lamda=", minlamda)
     return W
 
 #共轭梯度
 def Mode3_1():
-    A = X.T * X
-    B = X.T * T
+    A = X1.T * X1
+    B = X1.T * T1
     W = np.mat(np.ones(shape=(m + 1, 1)))
     R = W
     R = B - A * W
@@ -92,11 +116,9 @@ def Mode3_1():
         k = k + 1
     return W
 def Mode3_2():
-    lamda = math.exp(-8)
-    I = np.eye(m + 1)  # 加惩罚项
-    Ie = lamda * I
-    A = X.T*X + Ie
-    B = X.T * T
+    I = minlamda*np.eye(m + 1)  # 加惩罚项
+    A = X1.T*X1 + I
+    B = X1.T * T1
     W = np.mat(np.ones(shape=(m + 1, 1)))
     R = W
     R = B - A * W
@@ -112,6 +134,7 @@ def Mode3_2():
         beta = R.T * R * 1.0 / (R1.T * R1)
         P = R + beta[0, 0] * P
         k = k + 1
+    print(" Lamda=", minlamda)
     return W
 
 #第三部分
@@ -129,7 +152,7 @@ def draw():
     plt.figure()
     plt.xlabel("X axis")
     plt.ylabel("Y axis")
-    plt.scatter(a, b, c='c', alpha=0.4)
+    plt.scatter(a[0:divide], b[0:divide], c='c', alpha=0.4)
     x = np.linspace(0, 1, 100)  # 中间间隔100个元素
     y1 = np.sin(2 * np.pi * x)
     y2 = match(x)
